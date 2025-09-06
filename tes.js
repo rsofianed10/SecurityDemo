67a0184826a09041c10066fddab0ede05a347406
rsofianed10 <rsofianed10@gmail.com>
2025-09-06 23:46:10 +0200

    Fix

--- tes.js
+++ tes.js
@@ -2,20 +2,18 @@ const http = require("http");
 
 
 function createServer() {
-  const server = http.createServer((req, res) => {
-    const urlParams = new URL(req.url, "http://localhost");
-    const name = urlParams.searchParams.get("name") || "Invité";
+    const server = http.createServer((req, res) => {
+        const urlParams = new URL(req.url, "http://localhost");
+        const name = urlParams.searchParams.get("name") || "Invité";
 
-    // Ici, on insère directement la valeur de `name` dans le HTML
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<h1>Bonjour ${name}</h1>`);
-  });
+        // Ici, on insère directement la valeur de `name` dans le HTML
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(`<h1>Bonjour ${name}</h1>`);
+    });
 
-  server.listen(3000, () => {
-    console.log("Serveur vulnérable en écoute sur http://localhost:3000");
-  });  
-}  
+    server.listen(3000, () => {
+        console.log("Serveur vulnérable en écoute sur http://localhost:3000");
+    });
+}
+createServer();
 
-
-createServer();     
-  
