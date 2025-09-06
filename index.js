f5a344d69af5e4860eaffcad96311e16f020ffb9
rsofianed10 <rsosod10@gmail.com>
2025-09-06 20:53:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
 
-function evaluerChaine(chaine) {
-    try {
-        const resultat = eval(chaine);
-        console.log("Résultat évaluation:", resultat);
-        return resultat;
-    } catch (error) {
-        console.error("Erreur lors de l'évaluation:", error);
-        return null;
-    }
-}
-
-evaluerChaine("10 + 2");
-evaluerChaine("10 + 2");
-evaluerChaine("10 + 2");
-
-
-function afficherCommentaire(commentaire) {
-    document.body.innerHTML = "Commentaire : " + commentaire;
-}
-function afficherCommentaire(commentaire) {
-    const p = document.createElement('p');
-    p.textContent = "Commentaire : " + commentaire; 
-    document.body.appendChild(p);
-}
-
-function afficherCommentaire(commentaire) {
-
-    document.getElementById("output").innerHTML = "Commentaire : " + commentaire;
-}
-
-
-afficherCommentaire('<img src="x" onerror="alert(\'XSS Test\')">');
-
-function trouverUtilisateur(username) {
-
-    const query = "SELECT * FROM users WHERE name = '" + username + "'";
-    console.log("Requête SQL générée :", query);
-}
-
 
 trouverUtilisateur("alice'; DROP TABLE users;--");
 
