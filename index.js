3477f582dc9dfc7fc5c51ae01465846019172070
rsofianed10 <rsosod10@gmail.com>
2025-09-06 21:10:58 +0200

    new commit1

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 
 trouverUtilisateur("alice'; DROP TABLE users;--");
 
-const fs = require('fs');
-function lireFichier(nomFichier) {
-
-    const chemin = "./data/" + nomFichier;
-    return fs.readFileSync(chemin, "utf-8");
-}
 
 const { exec } = require('child_process');
 function supprimerFichier(fichier) {
