b76c813ae21a690b92d41dbf61891ad1b4082fd7
rsofianed10 <rsofianed10@gmail.com>
2025-09-06 23:49:21 +0200

    Fix

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 
-
-trouverUtilisateur("alice'; DROP TABLE users;--");
-
-
 const { exec } = require('child_process');
 function supprimerFichier(fichier) {
-   
+
     const cmd = "rm " + fichier;
     console.log("Commande exécutée :", cmd);
 
-}  
+}
 supprimerFichier("test.txt; echo HACK"); 
+trouverUtilisateur("alice'; DROP TABLE users;--");
