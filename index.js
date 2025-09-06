06c17439c32da1575102617a373334e2d194575a
rsofianed10 <rsofianed10@gmail.com>
2025-09-06 16:05:31 +0200

    Test sécurité

---
+++ index.js
@@ -0,0 +1,13 @@
+function evaluerChaine(chaine) {
+    try {
+        const resultat = eval(chaine);
+        console.log("Résultat évaluation:", resultat);
+        return resultat;
+    } catch (error) {
+        console.error("Erreur lors de l'évaluation:", error);
+        return null;
+    }
+}
+
+// Exemple d'utilisation
+evaluerChaine("10 + 2");
